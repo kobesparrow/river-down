1d87b6fecc90cdafe93d6b883c9584fdd5ad17c1
kobesparrow <markcollins.md@gmail.com>
2019-05-19 12:04:59 -0600

    Updates

---
+++ precache-manifest.bdbffbca8c05dca12834a7c8b6d7e68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebd391ad8bafc0d35e67",
+    "url": "/river-down/static/css/main.19937951.chunk.css"
+  },
+  {
+    "revision": "ebd391ad8bafc0d35e67",
+    "url": "/river-down/static/js/main.356fd232.chunk.js"
+  },
+  {
+    "revision": "e4df5eb281f44d635601",
+    "url": "/river-down/static/js/runtime~main.a39f293b.js"
+  },
+  {
+    "revision": "6fc7fb257c9f8447eb5f",
+    "url": "/river-down/static/js/2.b62bc261.chunk.js"
+  },
+  {
+    "revision": "5cf7226f17bc38fdfe7389ee2893d538",
+    "url": "/river-down/static/media/cataract-canyon-background.5cf7226f.jpg"
+  },
+  {
+    "revision": "17809e30c93591509f91c9469bc03b80",
+    "url": "/river-down/index.html"
+  }
+];
